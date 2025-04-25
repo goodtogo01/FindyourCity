04aec6c5e5a2f0acf9ba3ade67e7f9dc02cd58ea
kz1985 <khosruzzaman.ny@gmail.com>
2025-04-24 21:32:33 -0400

    fix: align Cypress baseUrl and live-server port (8080)

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-   baseUrl: 'http://localhost:5500',
+   baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
     require("cypress-mochawesome-reporter/plugin")(on);
     },
